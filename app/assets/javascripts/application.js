c3e33e4303633b361ceb6e1fc3d672c4015d28bd
gokart <sergykoretsfsp@gmail.com>
2017-06-21 12:50:09 +0300

    initial order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
-//= require bootstrap-sprockets
 //= require selectize
+//= require bootstrap-sprockets
 //= require tinymce-jquery
 //= require_tree .
 
